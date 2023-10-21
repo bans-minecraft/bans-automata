2e21d9cb2b4c8f837374f92d795629548a574e96
Blake Rain <blake.rain@blakerain.com>
2023-10-21 16:03:39 +0100

    fix: issues in `git-sync.lua` tool

--- tools/git-sync.lua
+++ tools/git-sync.lua
@@ -5,9 +5,9 @@ local REPO = "BlakeRain/bans-automata"
 
 local function getJson(path)
   local url = ("https://%s/api/v1/%s"):format(HOST, path)
-  local response = http.get(url, nil, false)
+  local response, err = http.get(url, nil, false)
   if not response then
-    print("Failed to get response from " .. url)
+    print("Failed to get response from " .. url .. ": " .. err)
     return nil
   end
 
@@ -21,20 +21,29 @@ local function isLuaFile(path)
 end
 
 local function getLuaPaths(path)
-  local url = ("%s/contents"):format(REPO);
+  local url = ("repos/%s/contents"):format(REPO);
   if path then
     url = ("%s/%s"):format(url, path)
   end
 
   local found = {}
   local response = getJson(url)
+  if not response then
+    print("Unable to fetch repository contents (path: " .. path .. ")")
+    return nil
+  end
+
   for _, obj in ipairs(response) do
     if obj.type == "file" and isLuaFile(obj.name) then
       table.insert(found, obj.path)
     end
 
     if obj.type == "dir" then
-      local sub = getLuaContents(obj.path)
+      local sub = getLuaPaths(obj.path)
+      if not sub then
+        return nil
+      end
+
       for _, file in ipairs(sub) do
         table.insert(found, file)
       end
@@ -46,8 +55,11 @@ end
 
 local function downloadLuaFiles(dest)
   local paths = getLuaPaths()
+  print(("Found %d Lua files in git repository"):format(#paths))
+
   for _, path in ipairs(paths) do
-    local url = ("%s/contents/%s"):format(REPO, path)
+    print(("Downloading: %s"):format(path))
+    local url = ("repos/%s/contents/%s"):format(REPO, path)
     local response = getJson(url)
     local file = fs.open(fs.combine(dest, path), "w")
     file.write(Base64.decode(response.content))
